f6a91fb1226c6161aba623c1996928f31756673c
tal-hason <thason@redhat.com>
2023-09-07 00:34:06 +0300

    sparky-cicdsparky-cicdsparky-cicd

--- src/app.js
+++ src/app.js
@@ -42,7 +42,7 @@ app.get('/', function (req, res) {
 
   var clientHostname = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
 
-  res.send(`Dont look at my tights |^| ,New Version, My Image is ${image}:${tag} , the Server is ${host} accessed from ${clientHostname} `);
+  res.send(`Dont look at my tights |^| @@,New Version, My Image is ${image}:${tag} , the Server is ${host} accessed from ${clientHostname} `);
 
   console.log(`Someone accessed me! --> from ${clientHostname}`)
 });
